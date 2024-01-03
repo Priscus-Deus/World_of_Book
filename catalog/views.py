d5930f28f685218d38597b5623cabff435cd127a
Kirill Kyznetsov <iantiquai@gmail.com>
2024-01-03 14:59:27 +0500

    chapter 9

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Book, BookInstance, Author
+
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Главная страница сайта 'Мир книг!'")
+    num_books = Book.object.all().count()
+    num_instances = BookInstance.object.all().count()
+
+    num_instances_available = BookInstance.object.filter(status__exact=2).count
+    num_authors = Author.object.count()
+
+    return render(request, 'index.html', context={'num_books': num_books, 'num_instances':num_instances,
+                                                  'num_instances_available': num_instances_available,
+                                                  'num_authors':num_authors})
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    #template_name = 'book_detail.html'
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
