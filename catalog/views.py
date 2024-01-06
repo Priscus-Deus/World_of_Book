87b961627b1ce58108319486f3c892e638604920
Kirill Kyznetsov <iantiquai@gmail.com>
2024-01-06 12:04:41 +0500

    05.01.2024

--- catalog/views.py
+++ catalog/views.py
@@ -11,10 +11,13 @@ def index(request):
 
     num_instances_available = BookInstance.object.filter(status__exact=2).count
     num_authors = Author.object.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     return render(request, 'index.html', context={'num_books': num_books, 'num_instances':num_instances,
                                                   'num_instances_available': num_instances_available,
-                                                  'num_authors':num_authors})
+                                                  'num_authors':num_authors,
+                                                  'num_visits': num_visits})
 
 
 class BookListView(generic.ListView):
